c3639b4b992cc9c9aedd88d64d0901747c99fbff
Guilherme Bussinger <guilhermepedro@ioasys.com.br>
2020-12-19 00:31:33 -0300

    test view

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
 import TestView from 'react-native-awesome-module';
 
 
@@ -9,16 +7,3 @@ export default function App() {
   return (<TestView/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
